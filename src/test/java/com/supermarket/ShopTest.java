9dbec1b9237e3644e6bc5e84f999746893aeb48c
L.Malinowski <lmalinowski@cyfrowypolsat.com>
2015-03-02 12:20:45 +0000

    test value adjustment

--- src/test/java/com/supermarket/ShopTest.java
+++ src/test/java/com/supermarket/ShopTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ShopTest {
-    private static final double DELTA = 0.00001;
+    private static final double DELTA = 0.1;
 
     @Test
     public void testCheckout() throws Exception {
@@ -21,22 +21,22 @@ public class ShopTest {
         Cart cart = new Cart();
         cart.addItem(new Item("Milk", 1.50, new BuyNForMPromo(3, 2)), 3);
         Receipt receipt = shop.checkout(cart);
-        assertEquals(3, receipt.getTotalPrice(), DELTA);
         System.out.println(receipt.toPrettyString());
+        assertEquals(3, receipt.getTotalPrice(), DELTA);
 
         cart = new Cart();
         cart.addItem(new Item("Toblerone chocolate", 5d, new BuyNForPercentPromo(2, 0.75)), 2);
+        System.out.println(receipt.toPrettyString());
         receipt = shop.checkout(cart);
         assertEquals(7.50, receipt.getTotalPrice(), DELTA);
-        System.out.println(receipt.toPrettyString());
 
         cart = new Cart();
         Item corkscrew = new Item("Corkscrew", null, null);
         cart.addItem(new Item("Bordeaux wine", 9.90d, new BuyNGetM4FreePromo(4, 1, corkscrew)), 4);
         receipt = shop.checkout(cart);
+        System.out.println(receipt.toPrettyString());
         assertEquals(1, (long)receipt.getItemCount(corkscrew));
         assertEquals(39.60, receipt.getTotalPrice(), DELTA);
-        System.out.println(receipt.toPrettyString());
 
         cart = new Cart();
         cart.addItem(new Item("Ketchup", 1.65d, null), 1);
@@ -45,8 +45,8 @@ public class ShopTest {
         cart.addItem(new Item("Milka chocolate", 1.11, new BuyNForPercentPromo(2, 0.75)), 3);
 
         receipt = shop.checkout(cart);
+        System.out.println(receipt.toPrettyString());
         assertEquals(1, (long)receipt.getItemCount(corkscrew));
         assertEquals(67.85, receipt.getTotalPrice(), DELTA);
-        System.out.println(receipt.toPrettyString());
     }
 }
